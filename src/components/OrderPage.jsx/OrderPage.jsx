a958e02f908f7d97af6677c825553f947d537709
NepheilS <batuhanfidan07@hotmail.com>
2024-06-19 12:01:28 +0300

    header added to order page

--- src/components/OrderPage.jsx/OrderPage.jsx
+++ src/components/OrderPage.jsx/OrderPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./OrderPage.css";
+import { useHistory } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="./Assets/mile1-assets/logo.svg" />
+    </div>
+  );
+}
+
+function MainOrder() {
+  return <div className="Order-container"></div>;
+}
 
 function OrderPage() {
-  return <div>deneme</div>;
+  return (
+    <>
+      <Header />
+      <MainOrder />
+    </>
+  );
 }
 
 export default OrderPage;
